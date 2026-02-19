668ab186ec6fd0516ad2e364e70bcb30a7e0a168
CahuichG17 <cawy2000@hotmail.com>
2026-02-19 11:18:38 -0500

    fix: add trailing slash to base path for correct asset URLs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://cahuichg17.github.io',
-  base: '/happy-dent',
+  base: '/happy-dent/',
 });
